f8dd6b069965d72697674756ab567630dba8364f
Prateek Kr Jha <prateekj1211@gmail.com>
2024-02-11 19:22:04 +0530

    sgregation off odd even

--- linked_list/Client.java
+++ linked_list/Client.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class PartitionList {
+public class Client {
     public static Scanner scn = new Scanner(System.in);
 
     public static Node partition(Node head, int partioningValue) {
@@ -23,15 +23,39 @@ public class PartitionList {
 
         return lesserHead.next;
     }
+
+    public static Node oddEven(Node head) {
+        Node oddHead = new Node(0);
+        Node odd = oddHead;
+        Node evenHead = new Node(0);
+        Node even = evenHead;
+
+        while(head != null) {
+            if(head.data % 2 == 0) {
+                even.next = head;
+                even = even.next;
+            } else {
+                odd.next = head;
+                odd = odd.next;
+            }
+            head = head.next;
+        }
+
+        odd.next = null;
+        even.next = oddHead.next;
+
+        return evenHead.next;
+    }
     public static void main(String[] args) {
-        int val = Integer.parseInt(args[0]);
+        // int val = Integer.parseInt(args[0]);
         List l1 = new List();
         while(scn.hasNext()) {
             l1.add(scn.nextInt());
         }
         l1.printAll();
         // while()
-        Node partionedList = partition(l1.head, val);
+        // Node partionedList = partition(l1.head, val);
+        Node partionedList = oddEven(l1.head);
 
         Node iterator = partionedList;
         while(iterator != null) {
